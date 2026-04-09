a916b2cf7acaffd36f3b8a780cbfebac7ce345d9
Jussi Jahkola <jjahkola@c1r6p10.hive.fi>
2026-04-09 12:39:12 +0300

    Added comments to clarify virtual and override keyword usage

--- ex01/ClapTrap.hpp
+++ ex01/ClapTrap.hpp
@@ -6,6 +6,18 @@
 	When working with inheritance, private members need to
 	be set to protected instead! Private members are invisible
 	to inheriting classes.
+
+	!virtual keyword tells compiler to check the exact subclass of
+	!an object pointed to by a base class pointer. Without it,
+	!calling a function by a base class pointer will default to
+	!the base class function. With it, the compiler will check
+	!the actual specific sub class and call the correct function
+	!virtual should be paired with override!
+	virtual void	attack(const std::string& target); <-parent class
+	void	attack(const std::string& target) override; <-child class
+
+	!destructors should always be virtual, otherwise child objects
+	!will be incompletely deleted if using base class pointers
 */
 
 class ClapTrap
